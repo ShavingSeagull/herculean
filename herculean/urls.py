f6d6ffd09a5aaeb57d273b68f971d19de402d8c0
Scott Kipp <scott.ecfx@gmail.com>
2018-03-19 22:30:25 +0000

    Added Home app and completed Index Page

--- herculean/urls.py
+++ herculean/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf.urls import url
+from home.views import index
 
 urlpatterns = [
     url('admin/', admin.site.urls),
+    url(r'^$', index, name="index"),
 ]
---
+++ static/js/script.js
@@ -0,0 +1,4 @@
+// Script for the carousel
+$('.carousel').carousel({
+      interval: 5000
+  });
