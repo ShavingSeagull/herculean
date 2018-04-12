6a148f8ab38f9d8da910ddc3210168757cd0588c
Scott Kipp <scott.ecfx@gmail.com>
2018-04-12 22:23:05 +0100

    Updated News page and added FontAwesome icons to Nav

--- news/urls.py
+++ news/urls.py
@@ -4,6 +4,5 @@ from .views import get_posts, add_post, post_content
 urlpatterns = [
     url(r'^$', get_posts, name='news'),
     url(r'^add-post/$', add_post, name='add-post'),
-    #url(r'^(?P<slug>\d+)/$', post_content, name='post-content'),
     url(r'(?P<slug>[\w-]+)/$', post_content, name='post-content'),
 ]
